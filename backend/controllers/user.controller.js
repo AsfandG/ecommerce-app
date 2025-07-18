944dfa6812cdcfd8b28e3760f670aa91a4fa0e8e
Asfandyar <asfandm17@gmail.com>
2025-07-19 01:17:36 +0500

    feat: add auth apis

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,5 +1,73 @@
-const loginUser = async (req, res) => {};
-const registerUser = async (req, res) => {};
-const adminLogin = async (req, res) => {};
+import User from "../models/user.model.js";
+import validator from "validator";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET_KEY);
+};
+
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const userExist = await User.findOne({ email });
+    if (userExist) {
+      return res.json({ success: false, message: "user already exists" });
+    }
+
+    if (!validator.isEmail(email)) {
+      return res.json({
+        success: false,
+        message: "please enter a valid email!",
+      });
+    }
+
+    if (password.length < 5) {
+      return res.json({
+        success: false,
+        message: "please enter strong password",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    const user = await newUser.save();
+    const token = createToken(user._id);
+
+    res.json({ success: true, token });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.json({ success: false, message: "user does not exist" });
+    }
+
+    const isPasswordMatched = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatched) {
+      return res.json({ success: false, message: "invalid credentials" });
+    }
+
+    const token = createToken(user._id);
+    res.status(200).json({ success: true, token });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+const adminLogin = async (req, res) => {
+  res.json({ message: "adming login" });
+};
 
 export { loginUser, registerUser, adminLogin };
